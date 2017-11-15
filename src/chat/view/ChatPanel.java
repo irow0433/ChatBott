e736567ab450f96ab8484d3cce1e28ff3f532a40
irow0433 <31513820+irow0433@users.noreply.github.com>
2017-11-15 09:35:26 -0700

    fixed the display of the chatbot

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,4 +1,5 @@
 package chat.view;
+import java.awt.Color;
 
 import javax.swing.*;
 
@@ -10,12 +11,45 @@ public class ChatPanel extends JPanel
 	private JButton chatButton;
 	private JTextField inputField;
 	private JTextArea chatArea;
-	private SpringLayout baseLayout;
+	private SpringLayout appLayout;
 	
 	public ChatPanel(ChatController appController)
 	{
 		super();
 		this.appController = appController;
+		chatButton = new JButton("chat");
+		chatArea = new JTextArea(10, 15);
+		inputField = new JTextField(20);
+		appLayout = new SpringLayout();
+		appLayout.putConstraint(SpringLayout.NORTH, chatArea, 0, SpringLayout.NORTH, this);
+		appLayout.putConstraint(SpringLayout.EAST, chatArea, 0, SpringLayout.EAST, chatButton);
+		appLayout.putConstraint(SpringLayout.WEST, inputField, 84, SpringLayout.WEST, this);
+		appLayout.putConstraint(SpringLayout.WEST, chatArea, 10, SpringLayout.WEST, this);
+		appLayout.putConstraint(SpringLayout.NORTH, inputField, 0, SpringLayout.NORTH, chatButton);
+		appLayout.putConstraint(SpringLayout.SOUTH, chatButton, -10, SpringLayout.SOUTH, this);
+		appLayout.putConstraint(SpringLayout.EAST, chatButton, -10, SpringLayout.EAST, this);
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
 	}
-
+	
+	private void setupListeners()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupPanel()
+	{
+		this.setBackground(Color.GREEN);
+		this.setLayout(appLayout);
+		this.add(chatButton);
+		this.add(inputField);
+		this.add(chatArea);
+	}
 }
