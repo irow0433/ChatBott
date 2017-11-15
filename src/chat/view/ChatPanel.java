61ab441bbe5a9dd234698b6da5831f289d4f462e
irow0433 <31513820+irow0433@users.noreply.github.com>
2017-11-15 09:15:20 -0700

    added chatpanel with stuff in it

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,5 +1,21 @@
 package chat.view;
 
-public class ChatPanel {
+import javax.swing.*;
+
+import chat.controller.ChatController;
+
+public class ChatPanel extends JPanel
+{
+	private ChatController appController;
+	private JButton chatButton;
+	private JTextField inputField;
+	private JTextArea chatArea;
+	private SpringLayout baseLayout;
+	
+	public ChatPanel(ChatController appController)
+	{
+		super();
+		this.appController = appController;
+	}
 
 }
